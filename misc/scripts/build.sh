2001ceccbb62b8cd7f1042f3becdb4e47d49ad21
Davius <66792063+Xdavius@users.noreply.github.com>
2024-09-16 16:21:01 +0200

    Update build.sh

--- misc/scripts/build.sh
+++ misc/scripts/build.sh
@@ -666,8 +666,8 @@ function install_deb() {
     if ((PACSTALL_INSTALL != 0)); then
         for pkg in "${replaces[@]}"; do
         	sudo dpkg -r --force-all "${pkg}"
-		 	sudo dpkg -i "${STAGEDIR:?}/${pacname}.deb"
         done
+		sudo dpkg -i "${STAGEDIR:?}/${pacname}.deb"
         # --allow-downgrades is to allow git packages to "downgrade", because the commits aren't necessarily a higher number than the last version
         if ! sudo -E apt-get install --reinstall "$STAGEDIR/$debname.deb" -y --allow-downgrades 2> /dev/null; then
             echo -ne "\t"
