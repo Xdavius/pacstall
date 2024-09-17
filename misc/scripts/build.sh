7e02261ed8107c742ce11c76a75e294880964936
Davius <66792063+Xdavius@users.noreply.github.com>
2024-09-17 16:18:13 +0200

    Update build.sh

--- misc/scripts/build.sh
+++ misc/scripts/build.sh
@@ -668,9 +668,18 @@ function install_deb() {
         	sudo dpkg -r --force-all "${pkg}" &> /dev/null
         done
         # --allow-downgrades is to allow git packages to "downgrade", because the commits aren't necessarily a higher number than the last version
-		 if sudo -E dpkg -i --force-all "${STAGEDIR}/${debname}.deb"; then
-    		sudo -E apt-get -f install -y
-	     else
+		 if  [[ "$debname" == *"dummy-builddeps = true"* ]]; then
+			if ! sudo -E apt-get install --reinstall "$STAGEDIR/$debname.deb" -y --allow-downgrades 2> /dev/null; then
+            echo -ne "\t"
+            fancy_message error $"Failed to install %s deb" "$pacname"
+            error_log 8 "install $pacname"
+            sudo dpkg -r --force-all "${gives:-$pacname}" 2> /dev/null
+            fancy_message info $"Cleaning up"
+            cleanup
+            exit 1
+		else   
+   			sudo -E dpkg -i "${STAGEDIR}/${debname}.deb"
+	  		if ! sudo -E apt-get -f install -y 2> /dev/null; then
             echo -ne "\t"
             fancy_message error $"Failed to install %s deb" "$pacname"
             error_log 8 "install $pacname"
