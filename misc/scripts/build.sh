c450243c2e3e7b5b2aa2a93bb6e77f972f4df40b
Davius <66792063+Xdavius@users.noreply.github.com>
2024-09-17 10:34:58 +0200

    Update build.sh

--- misc/scripts/build.sh
+++ misc/scripts/build.sh
@@ -690,7 +690,7 @@ function install_deb() {
         local combined_pinning=("${provides[@]}" "${gives:-${pacname}}")
         echo "Package: ${combined_pinning[*]}" | sudo tee "/etc/apt/preferences.d/${pacname//./-}-pin" > /dev/null
         echo "Pin: version *" | sudo tee -a "/etc/apt/preferences.d/${pacname//./-}-pin" > /dev/null
-        echo "Pin-Priority: -1" | sudo tee -a "/etc/apt/preferences.d/${pacname//./-}-pin" > /dev/null
+        echo "Pin-Priority: 1" | sudo tee -a "/etc/apt/preferences.d/${pacname//./-}-pin" > /dev/null
         return 0
     else
         sudo mv "$STAGEDIR/$debname.deb" "$PACDEB_DIR"
